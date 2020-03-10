bf809adf376c279ece0ce595da68fcd3a573d143
Yauheni Kozak <zhenikusss@gmail.com>
2020-03-10 23:13:01 +0300

    feat: implement function morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let encodedGroups = expr.match(/(.{1,10})/gim);
+    let morseGroups = [];
+
+    for (let i = 0; i < encodedGroups.length; i++) {
+        if (encodedGroups[i] === '**********') {
+            morseGroups[i] = [' '];
+        } else {
+            morseGroups[i] = [];
+            let encodedRow = encodedGroups[i].match(/(.{1,2})/gim);
+            for (let j = 0; j < encodedRow.length; j++) {
+                if (encodedRow[j] === '00') { 
+                    continue;
+                } 
+                if (encodedRow[j] === '10') {
+                    morseGroups[i].push('.');
+                } 
+                if (encodedRow[j] === '11') {
+                    morseGroups[i].push('-')
+                }
+            }
+        }
+    }
+
+    let result = '';
+    let morseArr = morseGroups.map(group => group.join(''));
+
+    for (let i = 0; i < morseArr.length; i++) {
+        if (morseArr[i] == ' ') {
+            result += ' ';
+        } else {
+            result += MORSE_TABLE[morseArr[i]];
+        }
+    }
+    return result;
 }
 
 module.exports = {
